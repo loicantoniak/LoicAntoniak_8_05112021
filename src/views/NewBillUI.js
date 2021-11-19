f4c258e56a3404cd435ed9704dd7971c5d186e14
Antoniak Loïc <loic.antoniak@gmail.com>
2021-11-19 11:30:51 +0100

    Fix bug NewBills : format de l'image

--- src/views/NewBillUI.js
+++ src/views/NewBillUI.js
@@ -56,6 +56,7 @@ export default () => {
                   <div class="col-half">
                     <label for="file" class="bold-label">Justificatif</label>
                     <input required type="file" class="form-control blue-border" data-testid="file" />
+                    <div class="error error-imageFormat">Votre justificatif doit être de format : jpg, jpeg ou png</div>
                   </div>
                 </div>
             </div>
